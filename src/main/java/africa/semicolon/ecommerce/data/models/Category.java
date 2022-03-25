0042c489cf1a2cd83726c6b6a620878caf3c6860
ajala ahmad <ahmadajala98@gmail.com>
2022-03-25 09:12:57 +0100

    Business Logic Exception

---
+++ src/main/java/africa/semicolon/ecommerce/data/models/Category.java
@@ -0,0 +1,5 @@
+package africa.semicolon.ecommerce.data.models;
+
+public enum Category {
+    PHONES, LAPTOPS, SHOES, BAGS, SHIRTS, WATER, DRINK;
+}
---
+++ src/main/java/africa/semicolon/ecommerce/web/exceptions/BusinessLogicException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.ecommerce.web.exceptions;
+
+public class BusinessLogicException extends Throwable{
+    public BusinessLogicException(String message){
+        super(message);
+    }
+}
