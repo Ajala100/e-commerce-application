a23e8f9746a42d413c91b46de734e752847d0976
ajala ahmad <ahmadajala98@gmail.com>
2022-03-25 09:18:23 +0100

    User not found exception class

---
+++ src/main/java/africa/semicolon/ecommerce/web/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.ecommerce.web.exceptions;
+
+public class UserNotFoundException extends Throwable{
+    public UserNotFoundException(String message){
+        super(message);
+    }
+}
